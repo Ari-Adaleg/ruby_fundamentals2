7d881db08214f27a3fef0d0f6e2ed28b8b5fff54
Ari Drookman <aga.drookman@gmail.com>
2019-12-12 19:48:23 -0500

    Updated exercise #8

--- exercise8.rb
+++ exercise8.rb
@@ -1,18 +1,16 @@
-def stats
-	for x in 1..3 do
-		puts "How far did person#{x} run (in metres)?"
+def stats(a)
+		puts "How far did person#{a} run (in metres)?"
 		distance = gets.to_f
-		puts "How long (in minutes) did person#{x} run take to run #{distance} metres?"
+		puts "How long (in minutes) did person#{a} run take to run #{distance} metres?"
 		mins = gets.to_f
 		secs = mins * 60
 		speed = distance/secs
 		return speed
-	end
 end
 
-speed1 = stats
-speed2 = stats
-speed3 = stats
+speed1 = stats("1")
+speed2 = stats("2")
+speed3 = stats("3")
 
 if speed3 > speed2 && speed3 > speed1
 puts "Person 3 was the fastest at #{speed3} m/s"
